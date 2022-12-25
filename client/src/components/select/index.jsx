ca6a1dcdee6b6f8f09fcb3fd468b25d24cc12b95
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2022-12-25 06:00:45 +0200

    selection init

--- client/src/components/select/index.jsx
+++ client/src/components/select/index.jsx
@@ -1,14 +1,39 @@
 import { AppContext } from "../../context/index";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import style from "./style.module.css";
+import "react-image-crop/dist/ReactCrop.css";
+import ReactCrop from "react-image-crop";
+import img from "../../globals/assets/0.jpg";
 
 const Select = () => {
   // fetch files from the context
   const { status, setStatus } = useContext(AppContext);
-
+  const [crop, setCrop] = useState({
+    unit: "%",
+    width: 100,
+    height: 100,
+    x: 0,
+    y: 0,
+  });
   // Methods
 
-  return <div className={style.container}></div>;
+  return (
+    <ReactCrop
+      crop={crop}
+      onDragStart={(e) => {
+        console.log(e);
+      }}
+      onComplete={(crop, perc) => {
+        console.log(crop, perc);
+      }}
+      onChange={(c) => {
+        setCrop(c);
+        // console.log(c);
+      }}
+    >
+      <img src={img} style={{ width: "800px" }} alt="img" />
+    </ReactCrop>
+  );
 };
 
 export default Select;
--- client/src/layout/index.jsx
+++ client/src/layout/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Select from "../components/select";
 import Test from "../components/test/index";
 
 const Layout = () => {
   return (
     <div>
-      <Test />
+      <Select />
     </div>
   );
 };
