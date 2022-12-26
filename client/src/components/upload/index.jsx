a364a0b778133b4167cca3c6b9455d90b5c0131b
Mazen-Aboulkhair <mazentarek1412@gmail.com>
2022-12-26 15:09:41 +0200

    img_upload

--- client/src/components/upload/index.jsx
+++ client/src/components/upload/index.jsx
@@ -1,14 +1,77 @@
 import { AppContext } from "../../context/index";
 import React, { useContext } from "react";
-import style from "./style.module.css";
+// import style from "./style.module.css";
 
-const Upload = () => {
-  // fetch files from the context
-  const { status, setStatus } = useContext(AppContext);
+// const Upload = () => {
+//   // fetch files from the context
+//   const { status, setStatus } = useContext(AppContext);
 
-  // Methods
+//   // Methods
 
-  return <div className={style.container}></div>;
-};
+//   return <div className={style.container}></div>;
+// };
 
-export default Upload;
+// export default Upload;
+
+import ReactDOM from "react-dom";
+import ImageUploading from "react-images-uploading";
+
+import "./style.css"
+
+function App() {
+  const [images, setImages] = React.useState([]);
+  const maxNumber = 69;
+  const onChange = (imageList, addUpdateIndex) => {
+    // data for submit
+    console.log(imageList, addUpdateIndex);
+    setImages(imageList);
+  };
+
+  return (
+    <div className="App">
+      <ImageUploading
+        multiple
+        value={images}
+        onChange={onChange}
+        maxNumber={maxNumber}
+        dataURLKey="data_url"
+        acceptType={["jpg"]}
+      >
+        {({
+          imageList,
+          onImageUpload,
+          onImageRemoveAll,
+          onImageUpdate,
+          onImageRemove,
+          isDragging,
+          dragProps
+        }) => (
+          // write your building UI
+          <div className="upload__image-wrapper">
+            <button
+              style={isDragging ? { color: "red" } : null}
+              onClick={onImageUpload}
+              {...dragProps}
+            >
+              Click or Drop here
+            </button>
+            &nbsp;
+            <button onClick={onImageRemoveAll}>Remove all images</button>
+            {imageList.map((image, index) => (
+              <div key={index} className="image-item">
+                <img src={image.data_url} alt="" width="100" />
+                <div className="image-item__btn-wrapper">
+                  <button onClick={() => onImageUpdate(index)}>Update</button>
+                  <button onClick={() => onImageRemove(index)}>Remove</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </ImageUploading>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
